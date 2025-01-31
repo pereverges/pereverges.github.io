e9a3a80535bc8ece6aaf3553b2d5477880ebaf19
pere verges <pere.verges@jediupc.com>
2025-01-31 10:26:22 -0800

    web

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,10 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="CheckInAlert" />
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
