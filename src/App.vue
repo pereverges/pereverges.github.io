ed266310041a8f2ca9cd2ae9a190aed01a3c2a9d
pere verges <pere.verges@jediupc.com>
2025-03-20 10:49:22 -0700

    website

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
+
+<!--script setup>
+
+import Header from './layouts/Header.vue';
+import Main from './layouts/Main.vue';
+import Footer from './layouts/Footer.vue';
+
+</script>
+
+<template>
+    <Header/>
+
+    <Main/>
+
+    <Footer />
+</template>
+
+<style scoped></style-->
+
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
---
+++ src/sections/HeroSection.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+
+<template>
+    <!-- Hero Section Start -->
+    <div class="container mx-auto px-5 overflow-x-hidden lg:overflow-x-visible">
+        <section class="grid grid-cols-none lg:grid-cols-2 pb-16 pt-8 items-center">
+            <div class="lg:w-5/6 order-1 lg:order-none ml-auto overflow-hidden rounded-lg">
+                <img class="z-10 w-full max-h-[60vh] object-contain rounded-[20px]" src="/images/CheckInAlert.jpeg" />
+            </div>
+
+            <div class="lg:w-5/6 order-1 lg:order-none">
+                <h1 class="text-4xl xl:text-5xl font-bold font-theme-heading text-center">The App</h1>
+                <h1 class="text-4xl xl:text-5xl font-bold font-theme-heading text-center">For Anyone Who</h1>
+                <h1 class="text-4xl xl:text-5xl font-bold font-theme-heading text-center">Lives Alone</h1>
+
+                <p class="lg:text-3xl mt-10 font-theme-content text-theme-grayish-blue text-center">Alerts Family & Friends</p>
+                <p class="lg:text-3xl font-theme-content text-theme-grayish-blue text-center">If Your Phone Doesn't Move</p>
+                <div class="flex justify-center mt-10">
+                    <LinkButton btn-type="primary" link="#download-section">iOS</LinkButton>
+                    <LinkButton btn-type="muted" link="#download-section">Android</LinkButton>
+                </div>
+            </div>
+
+
+        </section>
+        <p class="lg:text-3xl font-bold font-theme-heading text-center">No Actions Nedded! No Buttons To Push!</p>
+        <p class="lg:text-3xl font-theme-content text-theme-grayish-blue text-center">If your phone doesn't move in a time you set,</p>
+        <p class="lg:text-3xl font-theme-content text-theme-grayish-blue text-center">your friends and family will get a text so they</p>
+        <p class="lg:text-3xl font-theme-content text-theme-grayish-blue text-center">can check on you.</p>
+
+    </div>
+    <!-- Hero Section End -->
+</template>
